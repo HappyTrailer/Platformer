839805963386931e268ac67c12fcd29742db0185
HappyTrailer <285.zubchenko@gmail.com>
2017-01-17 15:05:00 +0200

    Add trigger for gate

--- Assets/Scripts/World/Trigger.cs
+++ Assets/Scripts/World/Trigger.cs
@@ -3,26 +3,38 @@ using System.Collections;
 
 public class Trigger : MonoBehaviour {
 
-    Animator anim;
-    public bool opened;
+    Animator animGate;
+    Animator animTrigger;
+
+    bool opened = false;
 
 	void Start () {
-        anim = GetComponent<Animator>();
+        animGate = GameObject.Find("Platform").GetComponent<Animator>();
+        animTrigger = GameObject.Find("Trigger").GetComponent<Animator>();
 	}
 	
 	void Update () {
-	    if(Input.GetKeyDown(KeyCode.E))
+        if (animTrigger.GetCurrentAnimatorStateInfo(0).IsName("TriggerOpen"))
         {
-            if(!opened)
-            {
-                opened = !opened;
-                anim.SetBool("Opened", true);
-            }
-            else
-            {
-                opened = !opened;
-                anim.SetBool("Opened", false);
-            }
+            animGate.SetBool("Opened", true);
+        }
+        else if (animTrigger.GetCurrentAnimatorStateInfo(0).IsName("TriggerClose"))
+        {
+            animGate.SetBool("Opened", false);
         }
 	}
+
+    void OnMouseDown()
+    {
+        if (!opened)
+        {
+            opened = !opened;
+            animTrigger.SetBool("Opened", true);
+        }
+        else
+        {
+            opened = !opened;
+            animTrigger.SetBool("Opened", false);
+        }
+    }
 }
