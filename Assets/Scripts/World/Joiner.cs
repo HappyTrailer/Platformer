0231abbc0cf6e00cb7e8283077125cc4f0cd113b
HappyTrailer <285.zubchenko@gmail.com>
2017-01-18 16:56:00 +0200

    Hook 50 %

---
+++ Assets/Scripts/World/Joiner.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Joiner : MonoBehaviour {
+
+    GameObject player;
+
+	void Start () {
+        player = GameObject.Find("Player1");
+	}
+	
+	void Update () {
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            RaycastHit2D hit = Physics2D.Raycast(Camera.main.ScreenToWorldPoint(Input.mousePosition), Vector2.zero);
+            if (hit.collider != null)
+            {
+                if(hit.collider.name.Contains("Platform"))
+                {
+                    player.GetComponent<DistanceJoint2D>().enabled = true;
+                    Vector3 pz = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+                    pz.z = 0;
+                    player.GetComponent<DistanceJoint2D>().connectedAnchor = new Vector2(pz.x, pz.y);
+                }
+            }
+        }
+	}
+}
